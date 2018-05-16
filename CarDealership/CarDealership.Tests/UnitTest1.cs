a4c2e4c31fd71e03983630dde0cd456110061892
LOWES\ayuen <adam.yuen@lowes.com>
2018-05-15 17:42:06 -0700

    testing unit test

---
+++ CarDealership/CarDealership.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit;
+using NUnit.Framework;
+
+namespace CarDealership.Tests
+{
+    [TestFixture(Category = "Car dealership service test")]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
