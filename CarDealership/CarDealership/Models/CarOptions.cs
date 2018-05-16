08b5de3acc907e0ea67b04cbea2422ad416a8ba4
LOWES\ayuen <adam.yuen@lowes.com>
2018-05-15 17:56:08 -0700

    Added n layer structure

---
+++ CarDealership/CarDealership/Models/CarOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace CarDealership.Models
+{
+    enum ColorOptions
+    {
+        Red,
+        White,
+        Gray,
+        Silver,
+        Black
+    }
+
+    public class CarOptions
+    {
+        ColorOptions Color { get; set; }
+
+        bool IsAutomatic { get; set; }
+
+        bool IsSunRoof { get; set; }
+
+        bool IsFourWheelDrive { get; set; }
+
+        bool IsLowMiles { get; set; }
+
+        bool IsPowerWindows { get; set; }
+
+        bool IsNavigation { get; set; }
+        
+        bool IsHeatedSeats { get; set; }
+    }
+}
